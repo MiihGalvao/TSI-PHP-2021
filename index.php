871bb0e256b7b7a7f81bc07525a21c5dd4174e47
Milena Galvão <milena.galvao@itboxonline.com.br>
2021-08-19 22:53:09 -0300

    Primeiro exemplo em PHP

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$nome = 'Milena Galvão';
+
+echo 'Olá Mundo !'; 
+
+if ( $nome == 'Milena') {
+
+    echo '<br><br> O nome é igual';
+}
+else {
+    echo '<br><br> O nome não é igual';
+}
+
+echo '<br><br>';
+
+for ( $i = 0; $i < 10; $i++) {
+    echo "Essa é a linha $i <br>";
+}
+
+echo '<br> 2+2 = ' . (2+2) . '!';
