3c1c5bd0d8ab0edd2d06759ce28ff51869be0bb2
Milena Galvão <milena.galvao@itboxonline.com.br>
2021-09-09 19:50:44 -0300

    Criação de Formulario, linkform, e recebe, inclusão de mais linhas no vetor

---
+++ recebe.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Seu e-mail é " . $_POST['email'] . " e sua senha é " . $_POST['password'];
+
+echo "<br><br>";
+
+echo "Seu e-mail é {$_POST['email']} e sua senha é {$_POST['password']}";
+
+echo "<br><br>";
+
+phpinfo();
