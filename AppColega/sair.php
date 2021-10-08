f2a39dda2f802fd8021853fc48cec6a72bf3b818
Milena Galvão <milena.galvao@itboxonline.com.br>
2021-10-08 13:43:56 -0300

    função para deslogar o usuario
    função para deslogar o usuario

---
+++ AppColega/sair.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//Controle de acesso, se o usuário estiver "logado", pode acessar o conteúdo
+session_start();
+
+if(!isset($_SESSION['id'])){
+
+    echo 'Faça o login antes!  <a href="login.html">login<a>';
+    exit();
+}
+//FIM controle de acesso
+
+session_destroy();
+
+header('Location: login.html');
